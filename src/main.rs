8417fcdc4a47d15e324acd54a73a736bc29f7cef
Case Wylie <cwylie@redhat.com>
2020-11-09 22:08:04 -0500

    new route

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ async fn test_route(req: HttpRequest) -> &'static str {
     "Test"
 }
 
+async fn liveness(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "PONG"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // let key = "LOG":
@@ -22,6 +27,7 @@ async fn main() -> std::io::Result<()> {
             // enable logger
             .wrap(middleware::Logger::default())
             .service(web::resource("/test").to(test_route))
+            .service(web::resource("/healthz").to(liveness))
             .service(web::resource("/").to(index))
     })
     .bind("0.0.0.0:8080")?
@@ -57,6 +63,26 @@ mod tests {
         };
 
         assert_eq!(response_body, r##"Hello world!"##);
+
+        Ok(())
+    }
+
+    #[actix_rt::test]
+    async fn test_liveness() -> Result<(), Error> {
+        let app = App::new().route("/", web::get().to(liveness));
+        let mut app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+        println!("{:?}",response_body);
+        assert_eq!(response_body, r##"PONG"##);
 
         Ok(())
     }
